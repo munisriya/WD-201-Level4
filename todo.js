f2a1e444ba32749c1db1657b9ed65101988ef946
munisriya <94538594+munisriya@users.noreply.github.com>
2022-11-19 09:13:42 +0530

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,51 +1,52 @@
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
-const todoList = () => {
-  const all = [];
-  const add = (todoItem) => {
-    all.push(todoItem);
-  };
-  const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
-  // yesterday
-  const overdue = () => {
-    return all.filter(
-      (todolist) => todolist.dueDate < new Date().toLocaleDateString("en-CA")
-    );
-  };
-  // today
-  const dueToday = () => {
-    return all.filter(
-      (todolist) => todolist.dueDate === new Date().toLocaleDateString("en-CA")
-    );
-  };
-  // tomorrow
-  const dueLater = () => {
-    return all.filter(
-      (todolist) => todolist.dueDate > new Date().toLocaleDateString("en-CA")
-    );
-  };
+describe("TodoList Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "todo1",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "test overdue item",
+      completed: false,
+      dueDate: new Date(
+        new Date().setDate(new Date().getDate() - 1)
+      ).toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "test duelater item",
+      completed: false,
+      dueDate: new Date(
+        new Date().setDate(new Date().getDate() + 1)
+      ).toLocaleDateString("en-CA"),
+    });
+  });
+  // task to complete
+  test("add", () => {
+    const count = all.length;
+    add({
+      title: "todo4",
+      completed: true,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(count + 1);
+  });
+  test("markAsComplete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
 
-  function toDisplayableList(list) {
-    return list
-      .map(
-        (todolist) =>
-          `${todolist.completed ? "[x]" : "[ ]"} ${todolist.title} ${
-            todolist.dueDate === new Date().toLocaleDateString("en-CA")
-              ? " "
-              : todolist.dueDate
-          }`
-      )
-      .join("\n");
-  }
-  return {
-    all,
-    add,
-    markAsComplete,
-    overdue,
-    dueToday,
-    dueLater,
-    toDisplayableList,
-  };
-};
-module.exports = todoList;
+  test("overdue_items", () => {
+    expect(overdue().length).toBe(1);
+    // expect(overdue_list[0]).toBe(all[0]);
+  });
+  test("dueToday_items", () => {
+    expect(dueToday().length).toBe(2);
+  });
+  test("dueLater_items", () => {
+    expect(dueLater().length).toBe(1);
+  });
+});
